8659b1956bca3c7e8083fb3d2175f02071aaf3db
Ashley Ruglys <ashley.ruglys@gmail.com>
2023-01-17 22:18:58 +0100

    Add seperate direct and indirect passes the lightmap.py

--- lightmap.py
+++ lightmap.py
@@ -2,28 +2,16 @@ import sys
 import mitsuba as mi
 import drjit as dr
 import argparse
+import os
+
 
 def generate_sphere_harmonics(value, normal):
     return [value, value * normal.x, value * normal.y, value * normal.z]
 
-def create_film(width, height):
-    film = mi.load_dict({
-        "type": "hdrfilm",
-        "width": width,
-        "height": height,
-        "rfilter": {
-            "type": "box",
-        },
-        "pixel_format": "rgb",
-        "component_format": "float32",
-    })
-    film.prepare([])
-    return film, film.create_block()
 
-def normalize_value(value, average):
-    value /= average
-    value = dr.select(dr.isnan(value), 0, value)
-    return value# * 0.5 + 0.5
+def dot_product(a, b):
+    return a.x * b.x + a.y * b.y + a.z * b.z
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("scene_path")
@@ -32,8 +20,16 @@ parser.add_argument("normals_path")
 parser.add_argument("seed", type=int)
 parser.add_argument("--sample-count", type=int, default=1024)
 parser.add_argument("--supersampling", type=int, default=4)
+parser.add_argument("--direct-only", action="store_true")
+parser.add_argument("--indirect-only", action="store_true")
 
 args = parser.parse_args()
+
+filename = f"{args.seed}_{args.sample_count}.exr"
+
+if os.path.exists(filename):
+    print(f"{filename} exists, skipping")
+    sys.exit(0)
 
 origin_sample_count = args.sample_count // (args.supersampling * args.supersampling)
 
@@ -53,14 +49,13 @@ width = positions.shape[1]
 num_channels = positions.shape[2]
 
 integrator = mi.load_dict({"type": "path"})
+indirect_integrator = mi.load_dict({"type": "path", "hide_emitters": True})
 
 sampler = mi.load_dict({"type": "independent", "sample_count": origin_sample_count})
 
 sampler.seed(args.seed, width * height * origin_sample_count)
 
-index = dr.repeat(
-    dr.arange(dr.llvm.UInt, width * height), origin_sample_count
-)
+index = dr.repeat(dr.arange(dr.llvm.UInt, width * height), origin_sample_count)
 
 pixel_offset = index * num_channels
 pixel_coord = mi.Vector3u(pixel_offset, pixel_offset + 1, pixel_offset + 2)
@@ -73,21 +68,42 @@ mask = dr.neq(normal.x, 0) | dr.neq(normal.y, 0) | dr.neq(normal.z, 0)
 coord = mi.Vector2u(index % width, index // width)
 coord = dr.float_array_t(coord)(coord)
 
-sample_dir = mi.warp.square_to_uniform_hemisphere(sampler.next_2d())
-
 interaction = mi.Interaction3f(
     t=0, time=0, p=position, n=normal, wavelengths=mi.Color0f()
 )
 
-sample_dir = mi.Frame3f(normal).to_world(sample_dir)
+spherical_harmonics = None
 
-ray = interaction.spawn_ray(sample_dir)
+if args.direct_only:
+    (ds, spec) = scene.sample_emitter_direction(
+        interaction, sampler.next_2d(), active=mask
+    )
+    mask &= dot_product(ds.d, normal) > 0
+    spherical_harmonics = generate_sphere_harmonics(spec, ds.d)
+elif args.indirect_only:
+    sample_dir = mi.warp.square_to_uniform_hemisphere(sampler.next_2d())
+    sample_dir = mi.Frame3f(normal).to_world(sample_dir)
+    ray = interaction.spawn_ray(sample_dir)
+    (spec, _, _) = indirect_integrator.sample(scene, sampler, ray, active=mask)
+    # Limit the effect of fireflies
+    max_value = 10
+    spec = dr.minimum(spec, max_value)
+    spherical_harmonics = generate_sphere_harmonics(spec, sample_dir)
 
-(spec, _, _) = integrator.sample(scene, sampler, ray, active=mask)
-
-spherical_harmonics = generate_sphere_harmonics(spec, sample_dir)
-
-film, image_block = create_film(width * 4 // args.supersampling, height // args.supersampling)
+film = mi.load_dict(
+    {
+        "type": "hdrfilm",
+        "width": width * 4 // args.supersampling,
+        "height": height // args.supersampling,
+        "rfilter": {
+            "type": "box",
+        },
+        "pixel_format": "rgb",
+        "component_format": "float32",
+    }
+)
+film.prepare([])
+image_block = film.create_block()
 
 for i, sh in enumerate(spherical_harmonics):
     image_block.put(
@@ -100,4 +116,4 @@ film.put_block(image_block)
 
 image = film.develop()
 
-mi.util.write_bitmap(f"{args.seed}_{args.sample_count}.exr", image)
+mi.util.write_bitmap(filename, image)
